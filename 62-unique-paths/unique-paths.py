3f5d9b88482eab40619bcf1fd151a1da64000fe0
Tamirat Dejene <tamirat.dejenie@gmail.com>
2025-09-29 11:03:03 +0300

    Time: 0 ms (100.00%) | Memory: 17.6 MB (98.35%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,13 +1,23 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        ways = [[0]*n for _ in range(m)]
+        # ways = [[0]*n for _ in range(m)]
 
+        # for r in range(m):
+        #     for c in range(n):
+        #         if r == 0 or c == 0:
+        #             ways[r][c] = 1
+        #         else:
+        #             ways[r][c] = ways[r - 1][c] + ways[r][c - 1]
+        
+        # return ways[m-1][n-1]
+
+        ways = [0] * n
         for r in range(m):
             for c in range(n):
                 if r == 0 or c == 0:
-                    ways[r][c] = 1
+                    ways[c] = 1
                 else:
-                    ways[r][c] = ways[r - 1][c] + ways[r][c - 1]
+                    ways[c] = ways[c - 1] + ways[c]
         
-        return ways[m-1][n-1]
+        return ways[n-1]
         
