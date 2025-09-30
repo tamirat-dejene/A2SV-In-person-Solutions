8a21912e6a6195c93e1119baebb368e9894179b1
Tamirat Dejene <tamirat.dejenie@gmail.com>
2025-09-30 13:50:45 +0300

    Time: 0 ms (100.00%) | Memory: 18.1 MB (25.35%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,16 +1,31 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # BU DP
+        N = len(s)
         longest_word = max(len(word) for word in wordDict)
         dictionary = set(wordDict)
-        store = defaultdict(bool)
 
+        dp = [False] * (N + 1)
+        dp[N] = True
+
+        for i in range(N - 1, -1, -1):
+            for j in range(i, min(i + longest_word, N)):
+                if s[i:j+1] in dictionary and dp[j + 1]:
+                    dp[i] = True
+        
+        return dp[0]
+
+
+
+        # TD DP
+
+        store = defaultdict(bool)
         def dfs(i):
-            if i == len(s):
+            if i == N:
                 return True
             
             if i not in store:
-            
-                for j in range(i, min(i + longest_word, len(s))):
+                for j in range(i, min(i + longest_word, N)):
                     if s[i:j+1] in dictionary and dfs(j + 1):
                         store[i] = True
                 
