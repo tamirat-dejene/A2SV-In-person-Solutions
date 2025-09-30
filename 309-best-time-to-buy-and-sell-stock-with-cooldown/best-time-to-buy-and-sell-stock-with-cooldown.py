bc7587ccc6b4388e3f9b93bedb2bfc501533e93f
Tamirat Dejene <tamirat.dejenie@gmail.com>
2025-09-30 14:47:19 +0300

    Time: 1260 ms (5.00%) | Memory: 18.1 MB (83.37%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,18 +1,32 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         N = len(prices)
-        profit = [-1] * N
+        # Bottom Up
+        dp = [0] * N
+
+        for b in range(N - 2, -1, -1):
+            for s in range(b + 1, N):
+                dp[b] = max(dp[b], prices[s] - prices[b] + (dp[s + 2] if s + 2 < N else 0))
+            
+            if b + 1 < N:
+                dp[b] = max(dp[b], dp[b + 1])
         
+        return dp[0]
+
+
+        # Top down
+        
+        profit = [-1] * N
         def dfs(b):
             if b >= N:
                 return 0
             
             if profit[b] == -1:
-                p = 0
+                profit[b] = 0
                 for s in range(b + 1, N):
-                    p = max(p, prices[s] - prices[b] + dfs(s + 2))
+                    profit[b] = max(profit[b], prices[s] - prices[b] + dfs(s + 2))
 
-                profit[b] = max(p, dfs(b + 1))
+                profit[b] = max(profit[b], dfs(b + 1))
             
             return profit[b]
         
