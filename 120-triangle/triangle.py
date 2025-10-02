e5d6e493d5d30efe2e0353efefe652221cb069e2
Tamirat Dejene <tamirat.dejenie@gmail.com>
2025-10-02 09:58:58 +0300

    Time: 6 ms (34.31%) | Memory: 19.1 MB (39.47%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -3,12 +3,11 @@ class Solution:
         N = len(triangle)
         
         for r in range(1, N):
-            row = triangle[r - 1]
-            triangle[r][0] += row[0]
+            triangle[r][0] += triangle[r - 1][0]
 
-            for c in range(1, len(row)):
-                triangle[r][c] += min(row[c], row[c - 1])
+            for c in range(1, len(triangle[r - 1])):
+                triangle[r][c] += min(triangle[r - 1][c], triangle[r - 1][c - 1])
 
-            triangle[r][-1] += row[-1]
+            triangle[r][-1] += triangle[r - 1][-1]
     
         return min(triangle[-1])
