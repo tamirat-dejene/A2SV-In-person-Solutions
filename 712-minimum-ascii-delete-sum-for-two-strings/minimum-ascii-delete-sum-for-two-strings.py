a7e88e39d997e357cca1e412be4a635f596f52e4
Tamirat Dejene <tamirat.dejenie@gmail.com>
2025-10-02 18:53:07 +0300

    Time: 663 ms (26.38%) | Memory: 33.5 MB (23.38%) - LeetSync

---
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
@@ -0,0 +1,49 @@
+class Solution:
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        m, n = len(s1), len(s2)
+        
+        store = [[-1] * n for _ in range(m)]
+        def dfs(l, r):
+            if l == m or r == n:
+                return sum([0] + [ord(s1[i]) for i in range(l, m)]) + sum([0] + [ord(s2[i]) for i in range(r, n)])
+            
+            if store[l][r] == -1:
+                if s1[l] == s2[r]:
+                    store[l][r] = dfs(l + 1, r + 1)
+                else:
+                    store[l][r] = min(
+                        ord(s1[l]) + dfs(l + 1, r),
+                        ord(s2[r]) + dfs(l, r + 1)
+                    )
+            return store[l][r]
+
+        return dfs(0, 0)
+
+
+        
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                if s1[i] == s2[j]:
+                    dp[i][j] = 1 + dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
+
+        # recover the lcs
+        i, j = 0, 0
+        common = []
+
+        while i < m and j < n:
+            if s1[i] == s2[j]:
+                common.append(s1[i])
+                i += 1
+                j += 1
+            else:
+                if dp[i + 1][j] >= dp[i][j + 1]:
+                    i += 1
+                else:
+                    j += 1
+        
+        print(common)
+        return sum(ord(c) for c in s1 + s2) - 2 * sum(ord(c) for c in common)
