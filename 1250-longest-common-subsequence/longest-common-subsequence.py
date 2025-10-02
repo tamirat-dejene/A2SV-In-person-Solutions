28accfb6615a01c3d4f517acffa39d97e61be19a
Tamirat Dejene <tamirat.dejenie@gmail.com>
2025-10-02 15:13:05 +0300

    Time: 350 ms (94.83%) | Memory: 25.6 MB (87.49%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -12,15 +12,19 @@ class Solution:
                                 |
         2   e ------------------|
         '''
+        dp = [0] * (m + 1)
 
         for i in range(n - 1, -1, -1):
+            curr = [0] * (m + 1)
             for j in range(m - 1, -1, -1):
                 if text1[j] == text2[i]:
-                    dp[i][j] = 1 + dp[i + 1][j + 1]
+                    curr[j] = 1 + dp[j + 1]
                 else:
-                    dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
+                    curr[j] = max(dp[j], curr[j + 1])
+            
+            dp = curr
 
-        return dp[0][0]
+        return dp[0]
 
         # Top Down
         store = [[-1]*n for _ in range(m)]
