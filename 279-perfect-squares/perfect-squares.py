1b9edffa2f0046fddfc76b26ea05d59e344d4ea8
Tamirat Dejene <tamirat.dejenie@gmail.com>
2025-10-02 09:30:36 +0300

    Time: 2493 ms (55.73%) | Memory: 18.2 MB (35.22%) - LeetSync

--- 279-perfect-squares/perfect-squares.py
+++ 279-perfect-squares/perfect-squares.py
@@ -1,19 +1,18 @@
 class Solution:
-    def __init__(self):
-        self.perfect = [i for i in range(1, 10001) if sqrt(i) == int(sqrt(i))]
-        self.dp = [1 if sqrt(i) == int(sqrt(i)) else i for i in range(10001)]
-
     def numSquares(self, n: int) -> int:
         if sqrt(n) == int(sqrt(n)): return 1
 
+        perfect = [i for i in range(1, n + 1) if sqrt(i) == int(sqrt(i))]
+        dp = [1 if sqrt(i) == int(sqrt(i)) else i for i in range(n + 1)]
+
         for num in range(1, n + 1):
-            for i in range(len(self.perfect)):
-                if num < self.perfect[i]:
+            for i in range(len(perfect)):
+                if num < perfect[i]:
                     break
 
-                self.dp[num] = min(self.dp[num], 1 + self.dp[num - self.perfect[i]])
+                dp[num] = min(dp[num], 1 + dp[num - perfect[i]])
 
-        return self.dp[n]
+        return dp[n]
 
 
 
