abe8581efab9ad9e53542bead92ce035721e4d08
Tamirat Dejene <tamirat.dejenie@gmail.com>
2025-10-09 10:39:41 +0300

    Time: 75 ms (49.05%) | Memory: 17.8 MB (74.52%) - LeetSync

--- 2546-number-of-subarrays-with-gcd-equal-to-k/number-of-subarrays-with-gcd-equal-to-k.py
+++ 2546-number-of-subarrays-with-gcd-equal-to-k/number-of-subarrays-with-gcd-equal-to-k.py
@@ -3,8 +3,11 @@ class Solution:
 
         N, ans = len(nums), 0
         for i in range(N):
+            if nums[i] % k != 0: continue
             gc = nums[i]
             for j in range(i, N):
+                if nums[j] % k != 0:
+                    break
                 gc = gcd(gc, nums[j])
                 if gc == k: ans += 1
         return ans
