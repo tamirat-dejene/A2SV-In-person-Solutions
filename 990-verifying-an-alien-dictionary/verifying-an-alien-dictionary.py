1816ea3ee0e93be0abad50cfc0630ce0545b2035
Tamirat Dejene <tamirat.dejenie@gmail.com>
2025-10-12 16:35:49 +0300

    Time: 68 ms (2.93%) | Memory: 18.7 MB (5.04%) - LeetSync

--- 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
@@ -1,70 +1,52 @@
 class TrieNode:
-    def __init__(self, size):
+    def __init__(self):
         self.is_end_of_word = False
-        self.alphabets = [False] * size
+        self.alphabets = [False] * 26
 
 class Trie:
-    def __init__(self, dictionary, alphabets):
-        self.idx = dictionary
-        self.size = len(dictionary)
-        self.root = TrieNode(len(dictionary))
+    def __init__(self, alphabets):
+        self.idx = {alph: i for i, alph in enumerate(alphabets)}
+        self.root = TrieNode()
         self.alphabets = alphabets
     
     def insert(self, word):
-        i, N = 0, len(word)
-        root, substr = self.root, []
+        i, N, root = 0, len(word), self.root
 
-        while i < N:
-            curr = ''.join(substr + [word[i]])
-            if curr in self.idx:
-                substr = [curr]
-            else:
-                # insert previous
-                idx = self.idx[''.join(substr)]
-                root.alphabets[idx] = root.alphabets[idx] or TrieNode(self.size)
-                root = root.alphabets[idx]
-                substr = [word[i]]
-            
-            if i == N - 1:
-                # insert previous
-                idx = self.idx[''.join(substr)]
-                root.alphabets[idx] = root.alphabets[idx] or TrieNode(self.size)
-                root = root.alphabets[idx]
-                root.is_end_of_word = True
-            
-            i += 1
-        
+        for c in word:
+            idx = self.idx[c]
+            root.alphabets[idx] = root.alphabets[idx] or TrieNode()
+            root = root.alphabets[idx]
+        root.is_end_of_word = True
+
     def inorder(self, curr):
-        alphabets = self.alphabets
-
         res = []
-        for i in range(self.size):
+        
+        for i in range(26):
             if curr.alphabets[i]:
                 suffixes = self.inorder(curr.alphabets[i])
 
                 # if the non leaf is end of word
                 if curr.alphabets[i].is_end_of_word and sum(1 for i in curr.alphabets[i].alphabets if i):
-                    res.append([alphabets[i]])
+
+                    res.append([self.alphabets[i]])
 
                 for suff in suffixes:
-                    res.append([alphabets[i]] + suff)
+                    res.append([self.alphabets[i]] + suff)
     
         return [[]] if not res else res
 
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        dictionary = {word: i for i, word in enumerate(order)}
         cnt = Counter(words)
 
-        trie = Trie(dictionary, order)
+        trie = Trie(order)
         for word in words:
             trie.insert(word)
         
-        io = trie.inorder(trie.root)
-        srted = []
+        io, srted = trie.inorder(trie.root), []
 
         for wo in io:
             wo = ''.join(wo)
             srted.extend([wo] * cnt[wo])
-
+        
         return words == srted
