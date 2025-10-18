d1f310e68d64cd3da80047997a89aa4e90382446
Tamirat Dejene <tamirat.dejenie@gmail.com>
2025-10-18 21:02:46 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (92.39%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.py
+++ 1095-two-city-scheduling/two-city-scheduling.py
@@ -1,22 +1,23 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        costs.sort(key=lambda cost: -abs(cost[0] - cost[1]))
-        a, b, lim = 0, 0, len(costs) // 2
-        tot_cost = 0
+        costs.sort(key=lambda itm: abs(itm[0] - itm[1]), reverse=True) # process max cost diff first
 
-        for ca, cb in costs:
-            if (ca <= cb and a < lim) or b >= lim:
-                tot_cost += ca
-                a += 1
+        ans, ca, cb = 0, 0, 0
+        tot = len(costs) // 2
+
+        for a, b in costs:
+            if a <= b:
+                if ca < tot:
+                    ca += 1
+                    ans += a
+                else:
+                    cb += 1
+                    ans += b
             else:
-                tot_cost += cb
-                b += 1
-        
-        return tot_cost
-
-
-
-
-        
-
-        
+                if cb < tot:
+                    cb += 1
+                    ans += b
+                else:
+                    ca += 1
+                    ans += a
+        return ans
