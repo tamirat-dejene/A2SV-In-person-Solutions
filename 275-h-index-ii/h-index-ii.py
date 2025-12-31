ce6b17222e8200c86d271d71ad98c670322082aa
Tamirat Dejene <tamirat.dejenie@gmail.com>
2025-12-31 11:51:54 +0300

    Time: 0 ms (100.00%) | Memory: 22.5 MB (87.90%) - LeetSync

--- 275-h-index-ii/h-index-ii.py
+++ 275-h-index-ii/h-index-ii.py
@@ -1,9 +1,15 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        s = len(citations)
-        for i, citation in enumerate(citations):
-            if citation >= s - i: return s - i
-        
-        return 0
+        N = len(citations)
+        l, r = -1, N
 
+        while l + 1 < r:
+            m = (l + r) // 2
+
+            if citations[m] >= N - m:
+                r = m
+            else:
+                l = m
+        
+        return N - r
         
